1aaac2b30e5098d6a26a07936d6b5866f750c386
argamManukyan <manoukian00@gmail.com>
2021-08-19 23:38:20 +0400

    task is finished

--- core/exceptions.py
+++ core/exceptions.py
@@ -1,3 +1,8 @@
 class MethodNotAllowedException(Exception):
     """ We handled the exception which is response from given endpoint,when i did sent call"""
     pass
+
+
+class JSONDecodeCustomError(Exception):
+    """ This exception we get when called client.send api  """
+    pass
--- core/views.py
+++ core/views.py
@@ -1,28 +1,40 @@
-import json
+import requests
 
-from django.views.decorators.csrf import csrf_exempt
-from jsonrpcclient.clients import http_client
+from jsonrpcclient.clients.http_client import HTTPClient
 
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 
-from .exceptions import MethodNotAllowedException
-
-
+from .exceptions import MethodNotAllowedException, JSONDecodeCustomError
 @csrf_exempt
 def login(request):
     if request.method == "POST":
-        client = http_client.HTTPClient('https://slb.medv.ru/api/v2/')
-        client.session.auth = ('test@test.test', settings.JSON_RPC_CLIENT_KEY)
+        """ Getting variables from settings.py and writing in the files """
+        with open('certificate.cert', 'w') as cert_file:
+            cert_file.write(settings.JSON_RPC_CERTIFICATE)
+        with open('key.key', 'w') as key_file:
+            key_file.write(settings.JSON_RPC_CLIENT_KEY)
 
-        update_req_data = []
-        for k,v in request.POST.items():
-            new_dict = {"jsonrpc": "2.0",}
-            new_dict[k] = v
-            update_req_data.append(json.dumps(new_dict))
+        """ Getting files paths """
+        key_file_path = str(settings.BASE_DIR) + f'\{key_file.name}'
+        cert_file_path = str(settings.BASE_DIR) + f'\{cert_file.name}'
+
+
         try:
-            resp = client.send(update_req_data, headers={'Content-Type': 'application/json-rpc'})
-        except Exception as e:
-            raise MethodNotAllowedException({'detail': 'method is not allowed'})
+            resp = requests.get('https://slb.medv.ru/api/v2/ ', cert=(r'%s' % cert_file_path, r'%s' % key_file_path),
+                                verify=True)
+            if resp.status_code not in [200, 201]:
+                raise MethodNotAllowedException({"detail": "Method not allowed"})
 
-    return render(request, 'index.html',)
+        except Exception as e:
+            print(e)
+
+        """ Exception handling , this exception raised but i all steps did with documentation """
+        try:
+            client = HTTPClient("http://cats.com")
+            client.request("ping")
+        except Exception as e:
+            raise JSONDecodeCustomError({'detail': 'Something went wrong'})
+
+    return render(request, 'index.html', )
